1c729e35009898dbba9ce226bf230e34a436d6c5
Gennady Tabala <gtabala@gmail.com>
2020-10-31 00:21:42 +0000

    wedwf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Message from './Message';
 import './App.css';
+import Chat from './Chat';
 
 function App() {
+
+const [messages, setMessages] = useState([]);
+const [kick, setKick] = useState(true);
+
+
+const askAPI = () => {
+  setKick(!kick);
+};
+
+useEffect(() => {
+    fetch("https://gtabala-chat-server.glitch.me/messages/")
+    .then(response => response.json())
+    .then(response => setMessages(response));
+  },[kick]
+);
+
+// Commented out due to Glitch blocking
+// setInterval(() => {
+//   setKick(!kick);
+// }, 30000);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+{
+  messages.map((item, ind) => <Message key={ind} msg={item} askAPI={askAPI} />)
+}
+      <Chat askAPI={askAPI} />
     </div>
   );
 }
