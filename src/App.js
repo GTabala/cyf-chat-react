59e36ee620dd0ca06b0d1e089dc9abf33794a985
Gennady Tabala <gtabala@gmail.com>
2020-10-31 00:12:47 +0000

    Revert "First"
    This reverts commit 632fc91205ab008a333bce1c49bc37885f9de8df.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
-// import logo from './logo.svg';
-import React, { useEffect, useState } from 'react';
-import Message from './Message';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Chat from './Chat';
 
 function App() {
-
-const [messages, setMessages] = useState([]);
-const [kick, setKick] = useState(true);
-
-
-const askAPI = () => {
-  setKick(!kick);
-};
-
-useEffect(() => {
-    fetch("https://gtabala-chat-server.glitch.me/messages/")
-    .then(response => response.json())
-    .then(response => setMessages(response));
-  },[kick]
-);
-
-// Commented out due to Glitch blocking
-// setInterval(() => {
-//   setKick(!kick);
-// }, 30000);
-
   return (
     <div className="App">
-
-{
-  messages.map((item, ind) => <Message key={ind} msg={item} askAPI={askAPI} />)
-}
-      <Chat askAPI={askAPI} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
